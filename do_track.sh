e3595aa11b43aac684ea24c213dc43f51856f9f5
Robert Nurnberg <robert.nurnberg@gmx.de>
2025-10-06 19:19:25 +0200

    ok

--- do_track.sh
+++ do_track.sh
@@ -22,6 +22,7 @@ echo "started at: " $(date)
 dbs="dump cdb"
 tcs="blitz rapid classical"
 lichess_prefix="lichess_db_standard_rated_"
+out=out.tmp
 
 for db in $dbs; do
   for tc in $tcs; do
@@ -47,7 +48,6 @@ months=$(
 )
 
 for month in $months; do
-  echo $month
   if [ $(grep -l "$month" litrack_*.csv | wc -l) == 6 ]; then
     continue
   fi
@@ -64,28 +64,32 @@ for month in $months; do
   echo "TC+Elo bucket filtering finished at: " $(date)
 
   for tc in $tcs; do
+    bench="N/A"
+    dump_results=
+    cdb_results=
     for elo in $elo_buckets; do
-      echo $elo
       bucket="${pgn_prefix}_${tc}_Elo$elo"
       pgn="$bucket.pgn"
       dump_output="${bucket}_dump.epd"
       cdb_output="${bucket}_cdb.epd"
       if [[ -f $pgn ]]; then
-        ./litrack2dump $pgn $dump_output
+        ./litrack2dump $pgn $dump_output >&$out
+        bench=$(grep Opened $out | awk '{print $4}')
         python litrack2cdb.py $dump_output -o $cdb_output
+        dump_results="${dump_results}","$(python litrack.py $dump_output)"
+        cdb_results="${cdb_results}","$(python litrack.py $cdb_output)"
+      else
+        dump_results="${dump_results}",
+        cdb_results="${cdb_results}",
       fi
     done
-  done
-
-  for db in $dbs; do
-    for tc in $tcs; do
-      csv=litrack_${tc}_$db.csv
-      if ! grep -q "$month" "$csv"; then
-        echo $month >>$csv
-        # python plotdata.py $csv
-        #  git add $csv # TODO add png files
-      fi
-    done
+    timestamp=$(date +%Y-%m-%d)
+    dump_csv=litrack_${tc}_dump.csv
+    cdb_csv=litrack_${tc}_cdb.csv
+    echo $month,$bench$dump_results >>$dump_csv
+    echo $month,$timestamp$cdb_results >>$cdb_csv
+    # python plotdata.py $csv
+    #  git add $csv # TODO add png files
   done
 done
 
